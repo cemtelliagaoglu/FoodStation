8a82181f142b3e33aeaf1ac9f8a2499914e0350c
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-02-22 12:49:28 +0300

    implement updated network services and remove unused libraries

--- FoodStation/Modules/Homepage/Interactor/HomepageInteractor.swift
+++ FoodStation/Modules/Homepage/Interactor/HomepageInteractor.swift
@@ -5,70 +5,58 @@
 //  Created by admin on 6.02.2023.
 //
 
-import FirebaseAuth
-import FirebaseDatabase
+import Foundation
 
 class HomepageInteractor: HomepagePresenterToInteractor{
-    // food data
-    // load user-cart data to calculate total price and return it to the view
+    
     var foodList: [Food]?
     
     var presenter: HomepageInteractorToPresenter?
     
     func requestLoadFoodList(){
-        if let currentUID = Auth.auth().currentUser?.uid{
-            // if user is logged in, requestAllFoods
-            APIService.requestAllFoodsInDatabase { foods, errorMessage in
-                if errorMessage != nil{
-                    self.presenter?.requestFailed(with: errorMessage!)
-                }else{
-                    if foods != nil{
-                        // Check database for liked foodIDs,
-                        // if foodID matches with food in foodList, set matching food(s) didLike true
-                        Database.database().reference().child("user-like").child(currentUID).observe(.value) { snapshot in
-                            var tempList = [Food]()
-                            for var food in foods!{
-                                if snapshot.hasChild(food.foodId){
-                                    food.didLike = true
-                                }
-                                tempList.append(food)
-                            }
-                            self.foodList = tempList
-                            self.presenter?.loadDataSucceed()
-                        }
-                    }
-                }
-            }
-        }
-    }
-    func requestFoodAmount(at index: Int) {
-        if let currentUser = Auth.auth().currentUser?.email{
-            APIService.requestUserCartInfo(for: currentUser) { foods in
-                if let cart = foods{
-                    guard let foodAtIndex = self.foodList?[index] else{
-                        print("FoodList At \(index), is empty")
-                        return
-                    }
-                    for foodInCart in cart{
-                        if foodInCart.foodName == foodAtIndex.foodName{
-                            let indexPath = IndexPath(row: index, section: 1)
-                            self.presenter?.updatedSuccessfully(at: indexPath)
-                        }
+        APIService.requestAllFoodsInDatabase { foods, errorMessage in
+            if errorMessage != nil{
+                self.presenter?.requestFailed(with: errorMessage!)
+            }else{
+                if foods != nil{
+                    // Check database for liked foodIDs,
+                    // if foodID matches with food in foodList, set matching food(s) didLike true
+                    FirebaseService.addLikesToFoodList(for: foods!) { updatedList in
+                        self.foodList = updatedList
+                        self.presenter?.loadedFoodListSuccessfully()
                     }
                 }
             }
         }
     }
     
-    func requestUpdateCartForFood(at index: Int, amount: Int) {
-        if let currentUser = Auth.auth().currentUser?.email{
-            guard let food = foodList?[index] else{ return }
-            APIService.requestAddToCart(for: currentUser, food: food, amount: amount) { response in
-                if response == "success"{
-                    self.presenter?.updatedSuccessfully()
-                }else{
-                    self.presenter?.requestFailed(with: response)
+    func requestFoodAmount(at indexPath: IndexPath) {
+
+        APIService.requestUserCartInfo{ foods in
+            if let cart = foods{
+                guard let foodAtIndex = self.foodList?[indexPath.row] else{
+                    print("FoodList at \(indexPath.row), is empty")
+                    return
                 }
+                for foodInCart in cart{
+                    if foodInCart.foodName == foodAtIndex.foodName{
+                        self.presenter?.checkedCartSuccessfully(at: indexPath, amountInCart: Int(foodInCart.foodAmount)!)
+                        return
+                    }
+                }
+            }
+            self.presenter?.checkedCartSuccessfully(at: indexPath, amountInCart: 0)
+        }
+    }
+    
+    func requestUpdateCartForFood(at indexPath: IndexPath, amount: Int) {
+        
+        guard let food = foodList?[indexPath.row] else{ return }
+        APIService.requestAddToCart(food: food, amount: amount) { response in
+            if response == "success"{
+                self.presenter?.updatedSuccessfully(at: indexPath)
+            }else{
+                self.presenter?.requestFailed(with: response)
             }
         }
     }
@@ -82,29 +70,18 @@ class HomepageInteractor: HomepagePresenterToInteractor{
     func foodInfo(at index: Int) -> Food? {
         return foodList?[index]
     }
-    func requestSignOut() {
-        do{
-            try Auth.auth().signOut()
-            presenter?.requestSignOutSucceed()
-        }catch{
-            print(error)
-            presenter?.requestFailed(with: error.localizedDescription)
-        }
-    }
-    func updateFoodLike(at index: Int, didLike: Bool) {
-        guard let foodID = foodList?[index].foodId else{ return }
+    
+    func updateFoodLike(at indexPath: IndexPath, didLike: Bool) {
         
-        if let currentUID = Auth.auth().currentUser?.uid{
-            FirebaseService.requestUpdateFoodLike(for: currentUID, foodID: foodID, didLike: didLike) { error in
-                if error != nil{
-                    self.presenter?.requestFailed(with: error!.localizedDescription)
-                }else{
-                    self.presenter?.updatedSuccessfully()
-                }
+        guard let foodID = foodList?[indexPath.row].foodId else{ return }
+        FirebaseService.requestUpdateFoodLike(foodID: foodID, didLike: didLike) { error in
+            if error != nil{
+                self.presenter?.requestFailed(with: error!.localizedDescription)
+            }else{
+                self.presenter?.updatedSuccessfully(at: indexPath)
             }
         }
     }
-    
     
 }
 
