7d2d0c8c8d7ceb260dddfdc193f86bd5afd10857
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-02-22 12:54:16 +0300

    configure cells and implement FoodCellDelegate methods

--- FoodStation/Modules/Like/View/LikeVC.swift
+++ FoodStation/Modules/Like/View/LikeVC.swift
@@ -34,12 +34,17 @@ class LikeVC: UIViewController{
 //MARK: - CollectionView Methods
 extension LikeVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return presenter?.numberOfItems() ?? 1
+        return presenter?.numberOfItems() ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! FoodCell
-        
+        cell.viewingMode = .like
+        cell.delegate = self
+        cell.indexPath = indexPath
+        guard let food = presenter?.foodForCell(at: indexPath.row) else{ return cell}
+        cell.food = food
+        cell.didLike = food.didLike
         return cell
     }
     
@@ -54,6 +59,7 @@ extension LikeVC: LikePresenterToView{
     
     func configUI() {
         view.backgroundColor = UIColor(named: "bgColor2")
+        navigationItem.title = "Liked Foods"
         
         collectionView.register(FoodCell.self, forCellWithReuseIdentifier: cellIdentifier)
         collectionView.dataSource = self
@@ -71,7 +77,7 @@ extension LikeVC: LikePresenterToView{
     }
     
     func reloadData() {
-        
+        collectionView.reloadData()
     }
     
     func showErrorMessage(_ errorMessage: String) {
@@ -79,5 +85,14 @@ extension LikeVC: LikePresenterToView{
         
         alert.addAction(UIAlertAction(title: "Close", style: .cancel))
         present(alert, animated: true)
+    }
+}
+//MARK: - FoodCellDelegate
+extension LikeVC: FoodCellDelegate{
+    func foodAmountDidChange(indexPath: IndexPath, newValue: Int) {
+        // no need to use
+    }
+    func likeButtonTapped(indexPath: IndexPath, didLike: Bool) {
+        presenter?.likeButtonTapped(at: indexPath.row)
     }
 }
