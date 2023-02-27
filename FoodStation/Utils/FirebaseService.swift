3318e512e138bfb1f781e5e8f2de1d2e70757e80
Cem Telliagaoglu <cem.telliagaoglu@gmail.com>
2023-02-27 17:22:40 +0300

    update signUp method, add "user-info" and "order-history" methods

--- FoodStation/Utils/FirebaseService.swift
+++ FoodStation/Utils/FirebaseService.swift
@@ -76,9 +76,10 @@ struct FirebaseService{
             completion(error)
         }
     }
-    static func requestSignUp(email: String, password: String, completion: @escaping ((Error?) -> ())){
+    static func requestSignUp(email: String, password: String, completion: @escaping ((String?, Error?) -> ())){
         Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-            completion(error)
+            let uid = authResult?.user.uid
+            completion(uid, error)
         }
     }
     static func requestSignOut(completion: @escaping((Error?) -> ())){
@@ -98,5 +99,63 @@ struct FirebaseService{
     }
     static func requestUserUID(completion: @escaping((String?) -> ())){
         completion(Auth.auth().currentUser?.uid)
+    }
+    static func createNewUserInfo(uid: String, name: String, address: String, card_number: String){
+        
+        let dictionary = ["name": name, "address": address, "card_number": card_number]
+        Database.database().reference().child("user-info").child(uid).setValue(dictionary)
+    }
+    static func requestLoadUserInfo(completion: @escaping(([String:AnyObject]) -> ())){
+        if let currentUID = Auth.auth().currentUser?.uid{
+            // user-info --> name, address, card number
+            Database.database().reference().child("user-info").child(currentUID).observe(.value) { snapshot in
+                guard let dictionary = snapshot.value as? Dictionary<String,AnyObject> else{ return }
+                completion(dictionary)
+            }
+        }
+    }
+    static func requestUpdateUserAddress(newAddress: String){
+        if let currentUID = Auth.auth().currentUser?.uid{
+            // user-info --> name, address, card number
+            Database.database().reference().child("user-info").child(currentUID).updateChildValues(["address": newAddress])
+        }
+    }
+    static func requestUpdateUserName(newName: String){
+        if let currentUID = Auth.auth().currentUser?.uid{
+            // user-info --> name, address, card number
+            Database.database().reference().child("user-info").child(currentUID).updateChildValues(["name": newName])
+        }
+    }
+    static func requestUpdateUserCreditCard(newCardNumber: String){
+        if let currentUID = Auth.auth().currentUser?.uid{
+            Database.database().reference().child("user-info").child(currentUID).updateChildValues(["card_number": newCardNumber])
+        }
+    }
+    static func requestSaveOrder(cart: [FoodInCart], price: Int, address: String, cardNumber: String,date: String, completion: @escaping((Error?) -> ()) ){
+        // order-history -> uid -> order-id -> address: "" , details: [FoodName,FoodAmount, FoodPrice, FoodImageURL],
+        if let currentUID = Auth.auth().currentUser?.uid{
+            let orderRef = Database.database().reference().child("order-history").child(currentUID).childByAutoId()
+            
+            var detailsArray: [String: AnyObject] = [:]
+            for index in 0..<cart.count{
+                let food = cart[index]
+                let foodDetails: [String: String] = ["food_name": food.foodName,
+                                                     "food_price": food.foodPrice,
+                                                     "food_amount": food.foodAmount,
+                                                     "food_image_name": food.foodImageName
+                                                    ]
+                detailsArray["food\(index)"] = foodDetails as AnyObject
+            }
+            
+            let dictionary: [String: Any] = ["date": date,"address": address, "price": price, "card_number": cardNumber,"details": detailsArray]
+            orderRef.setValue(dictionary) { error, ref in
+               completion(error)
+            }
+        }
+    }
+    static func requestOrderHistory(){
+        if let currentUID = Auth.auth().currentUser?.uid{
+            
+        }
     }
 }
